a74c7faf7256e21f057f7f2ed91c113c5aa9179c
Varun Mendre <varunmm0404@gmail.com>
2025-09-12 07:14:06 +0530

    Integrate backend with Express: add directory & file CRUD APIs using fs/promises and stream handling

--- server/server.js
+++ server/server.js
@@ -1,64 +1,74 @@
 import express from "express";
 import { createWriteStream } from "fs";
-import { readdir, rm, rename } from "fs/promises";
+import { mkdir, readdir, rename, rm, stat } from "fs/promises";
+import cors from "cors";
 
 const app = express();
-const port = 4000;
 
 app.use(express.json());
+app.use(cors());
 
-// Enabling CORS
-app.use((req, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Headers": "*",
-  });
-  next();
+// Read
+app.get("/directory/?*?", async (req, res) => {
+  const { "0":dirname } = req.params;
+  
+  const fullDirPath = `./storage/${dirname ? dirname : ""}`;
+  const filesList = await readdir(fullDirPath);
+  const resData = [];
+  for (const item of filesList) {
+    const stats = await stat(`${fullDirPath}/${item}`);
+    resData.push({ name: item, isDirectory: stats.isDirectory() });
+  }
+  res.json(resData);
+});
+
+// Create Directory
+app.post("/directory/?*", async (req, res) => {
+  const { 0: dirname } = req.params;
+  try {
+    await mkdir(`./storage/${dirname}`);
+    res.json({ message: "Directory Created!" });
+  } catch (err) {
+    res.json({ err: err.message });
+  }
 });
 
 // Create
-app.post("/:filename", (req, res) => {
-  const writeStream = createWriteStream(`./storage/${req.params.filename}`);
+app.post("/files/*", (req, res) => {
+  const writeStream = createWriteStream(`./storage/${req.params[0]}`);
   req.pipe(writeStream);
   req.on("end", () => {
     res.json({ message: "File Uploaded" });
   });
 });
 
-// Read
-app.get("/", async (req, res) => {
-  const filesList = await readdir("./storage");
-  res.json(filesList);
-});
+app.get("/files/*", (req, res) => {
+  const { 0: filePath } = req.params;
 
-app.get("/:filename", (req, res) => {
-  const { filename } = req.params;
   if (req.query.action === "download") {
     res.set("Content-Disposition", "attachment");
   }
-
-  res.sendFile(`${import.meta.dirname}/storage/${filename}`);
+  res.sendFile(`${import.meta.dirname}/storage/${filePath}`);
 });
 
 // Update
-app.patch("/:filename", async (req, res) => {
-  const { filename } = req.params;
-  await rename(`./storage/${filename}`, `./storage/${req.body.newFilename}`);
+app.patch("/files/*", async (req, res) => {
+  const { 0: filePath } = req.params;
+  await rename(`./storage/${filePath}`, `./storage/${req.body.newFilename}`);
   res.json({ message: "Renamed" });
 });
 
 // Delete
-app.delete("/:filename", async (req, res) => {
-  const { filename } = req.params;
-  const filePath = `./storage/${filename}`;
+app.delete("/files/*", async (req, res) => {
+  const { 0: filePath } = req.params;
   try {
-    await rm(filePath);
+    await rm(`./storage/${filePath}`,{recursive:true});
     res.json({ message: "File Deleted Successfully" });
   } catch (err) {
     res.status(404).json({ message: "File Not Found!" });
   }
 });
+
 app.listen(4000, () => {
-  console.log(`Server Started: Listening on port: ${port}...`);
+  console.log(`Server Started`);
 });
