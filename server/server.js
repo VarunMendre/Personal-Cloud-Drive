de14d0e663cd930146b7ef1447d50025e68e6e5d
Varun Mendre <varunmm0404@gmail.com>
2025-09-12 09:12:16 +0530

    fix: prevent path traversal vulnerability in file download endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createWriteStream } from "fs";
 import { mkdir, readdir, rename, rm, stat } from "fs/promises";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -9,22 +10,27 @@ app.use(express.json());
 app.use(cors());
 
 // Read
-app.get("/directory/?*?", async (req, res) => {
-  const { "0":dirname } = req.params;
-  
+app.get("/directory/?*", async (req, res) => {
+  const dirname = path.join("/", req.params[0]);
   const fullDirPath = `./storage/${dirname ? dirname : ""}`;
-  const filesList = await readdir(fullDirPath);
-  const resData = [];
-  for (const item of filesList) {
-    const stats = await stat(`${fullDirPath}/${item}`);
-    resData.push({ name: item, isDirectory: stats.isDirectory() });
+
+  try {
+    const filesList = await readdir(fullDirPath);
+    const resData = [];
+
+    for (const item of filesList) {
+      const stats = await stat(`${fullDirPath}/${item}`);
+      resData.push({ name: item, isDirectory: stats.isDirectory() });
+    }
+    res.json(resData);
+  } catch (err) {
+    res.json({ error: err.message });
   }
-  res.json(resData);
 });
 
 // Create Directory
 app.post("/directory/?*", async (req, res) => {
-  const { 0: dirname } = req.params;
+  const dirname = path.join("/", req.params[0]);
   try {
     await mkdir(`./storage/${dirname}`);
     res.json({ message: "Directory Created!" });
@@ -35,34 +41,40 @@ app.post("/directory/?*", async (req, res) => {
 
 // Create
 app.post("/files/*", (req, res) => {
-  const writeStream = createWriteStream(`./storage/${req.params[0]}`);
+  const filePath = path.join("/", req.params[0]);
+  const writeStream = createWriteStream(`./storage/${filePath}`);
   req.pipe(writeStream);
   req.on("end", () => {
     res.json({ message: "File Uploaded" });
   });
 });
 
+// Path traversal Vulnerability
 app.get("/files/*", (req, res) => {
-  const { 0: filePath } = req.params;
-
-  if (req.query.action === "download") {
-    res.set("Content-Disposition", "attachment");
-  }
-  res.sendFile(`${import.meta.dirname}/storage/${filePath}`);
+  const filePath = path.join("/", req.params[0]);
+    if (req.query.action === "download") {
+      res.set("Content-Disposition", "attachment");
+    }
+    res.sendFile(`${import.meta.dirname}/storage/${filePath}`,(err) => {
+        if(err) {
+          res.json({error: "file not Found"})
+        }
+      });
+  
 });
 
 // Update
 app.patch("/files/*", async (req, res) => {
-  const { 0: filePath } = req.params;
+  const filePath = path.join("/", req.params[0]);
   await rename(`./storage/${filePath}`, `./storage/${req.body.newFilename}`);
   res.json({ message: "Renamed" });
 });
 
 // Delete
 app.delete("/files/*", async (req, res) => {
-  const { 0: filePath } = req.params;
+  const filePath = path.join("/", req.params[0]);
   try {
-    await rm(`./storage/${filePath}`,{recursive:true});
+    await rm(`./storage/${filePath}`, { recursive: true });
     res.json({ message: "File Deleted Successfully" });
   } catch (err) {
     res.status(404).json({ message: "File Not Found!" });
