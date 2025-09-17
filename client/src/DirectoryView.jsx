7f5800aed6f7ed9c016787372e75a5bf92c94774
Varun Mendre <varunmm0404@gmail.com>
2025-09-17 06:41:19 +0530

    feat(vfs): implement virtual file system with support for directory & file operations

--- client/src/DirectoryView.jsx
+++ client/src/DirectoryView.jsx
@@ -4,24 +4,29 @@ import { Link, useParams } from "react-router-dom";
 function DirectoryView() {
   const BASE_URL = "http://localhost:4000";
   const [directoryItems, setDirectoryItems] = useState([]);
+  const [directoriesList, setDirectoriesList] = useState([]);
+  const [filesList, setFilesList] = useState([]);
   const [progress, setProgress] = useState(0);
   const [newFilename, setNewFilename] = useState("");
   const [newDirname, setNewDirname] = useState("");
-  const { "*": dirPath } = useParams();
+  const { dirId } = useParams();
 
   async function getDirectoryItems() {
-    const response = await fetch(`${BASE_URL}/directory/${dirPath}`);
+    const response = await fetch(`${BASE_URL}/directory/${dirId || ""}`);
     const data = await response.json();
-    setDirectoryItems(data);
+    // setDirectoryItems(data);
+    setDirectoriesList(data.directories);
+    setFilesList(data.files);
   }
   useEffect(() => {
     getDirectoryItems();
-  }, [dirPath]);
+  }, [dirId]);
 
   async function uploadFile(e) {
     const file = e.target.files[0];
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", `${BASE_URL}/files/${dirPath}/${file.name}`, true);
+    xhr.open("POST", `${BASE_URL}/file/${dirId || ""}`, true);
+    xhr.setRequestHeader("filename", file.name);
     xhr.addEventListener("load", () => {
       console.log(xhr.response);
       getDirectoryItems();
@@ -33,11 +38,20 @@ function DirectoryView() {
     xhr.send(file);
   }
 
-  async function handleDelete(filename) {
-    const response = await fetch(`${BASE_URL}/files/${dirPath}/${filename}`, {
+  async function handleDelete(fileId) {
+    const response = await fetch(`${BASE_URL}/file/${fileId}`, {
       method: "DELETE",
     });
     const data = await response.text();
+    console.log(data);
+    getDirectoryItems();
+  }
+
+  async function handleDeleteDir(dirID) {
+    const response = await fetch(`${BASE_URL}/directory/${dirID}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
     console.log(data);
     getDirectoryItems();
   }
@@ -47,18 +61,28 @@ function DirectoryView() {
     setNewFilename(oldFilename);
   }
 
-  async function saveFilename(oldFilename) {
-    setNewFilename(oldFilename);
-    const response = await fetch(
-      `${BASE_URL}/files/${dirPath}/${oldFilename}`,
-      {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ newFilename: `${dirPath}/${newFilename}` }),
-      }
-    );
+  async function saveFilename(fileId) {
+    const response = await fetch(`${BASE_URL}/file/${fileId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newFilename }),
+    });
+    const data = await response.text();
+    console.log(data);
+    setNewFilename("");
+    getDirectoryItems();
+  }
+
+  async function saveDir(dirId) {
+    const response = await fetch(`${BASE_URL}/directory/${dirId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newDirName: newFilename }),
+    });
     const data = await response.text();
     console.log(data);
     setNewFilename("");
@@ -67,11 +91,12 @@ function DirectoryView() {
 
   async function handleCreateDirectory(e) {
     e.preventDefault();
-    const url = `${BASE_URL}/directory${
-      dirPath ? "/" + dirPath : ""
-    }/${newDirname}`;
+    const url = `${BASE_URL}/directory/${dirId || ""}`;
     const response = await fetch(url, {
       method: "POST",
+      headers: {
+        dirname: newDirname,
+      },
     });
     const data = await response.json();
     console.log(data);
@@ -97,24 +122,30 @@ function DirectoryView() {
         />
         <button>Create Folder</button>
       </form>
-      {directoryItems.map(({ name, isDirectory }, i) => (
-        <div key={i}>
-          {name} {isDirectory && <Link to={`./${name}`}>Open</Link>}
-          {!isDirectory && (
-            <a href={`${BASE_URL}/files/${dirPath}/${name}?action=open`}>
-              Open
-            </a>
-          )}{" "}
-          {!isDirectory && (
-            <a href={`${BASE_URL}/files/${dirPath}/${name}?action=download`}>
-              Download
-            </a>
-          )}
+      {directoriesList.map(({ name, id }) => (
+        <div key={id}>
+          {name} <Link to={`/directory/${id}`}>Open</Link>{" "}
           <button onClick={() => renameFile(name)}>Rename</button>
-          <button onClick={() => saveFilename(name)}>Save</button>
+          <button onClick={() => saveDir(id)}>Save</button>
           <button
             onClick={() => {
-              handleDelete(name);
+              handleDeleteDir(id);
+            }}
+          >
+            Delete
+          </button>
+          <br />
+        </div>
+      ))}
+      {filesList.map(({ name, id }) => (
+        <div key={id}>
+          {name} <a href={`${BASE_URL}/file/${id}`}>Open</a>{" "}
+          <a href={`${BASE_URL}/file/${id}?action=download`}>Download</a>
+          <button onClick={() => renameFile(name)}>Rename</button>
+          <button onClick={() => saveFilename(id)}>Save</button>
+          <button
+            onClick={() => {
+              handleDelete(id);
             }}
           >
             Delete
--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -1,56 +1,77 @@
 import express from "express";
 import { createWriteStream } from "fs";
-import { rename, rm } from "fs/promises";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import { rename, rm, writeFile } from "fs/promises";
+import path from "path";
+import directoriesData from '../directoriesDB.json' with {type: "json"}
+import filesData from '../filesDB.json' with {type: "json"}
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const STORAGE_PATH = join(__dirname, "../storage");
 
 const router = express.Router();
 
 // Create
-router.post("/*", (req, res) => {
-  const filePath = req.params[0];
-  const writeStream = createWriteStream(join(STORAGE_PATH, filePath));
+router.post("/:parentDirId?", (req, res) => {
+  const parentDirId = req.params.parentDirId || directoriesData[0].id
+  const filename = req.headers.filename
+  const id = crypto.randomUUID();
+  const extension = path.extname(filename);
+  const fullFileName = `${id}${extension}`;
+  const writeStream = createWriteStream(`./storage/${fullFileName}`);
   req.pipe(writeStream);
-  req.on("end", () => {
+  req.on("end", async () => {
+    filesData.push({
+      id,
+      extension,
+      name: filename,
+      parentDirId,
+    })
+    const parentDirData = directoriesData.find((directoryData) => directoryData.id === parentDirId)
+    parentDirData.files.push(id)
+    await writeFile('./filesDB.json', JSON.stringify(filesData))
+    await writeFile('./directoriesDB.json', JSON.stringify(directoriesData))
     res.json({ message: "File Uploaded" });
   });
 });
 
-// Read / Download
-router.get("/*", (req, res) => {
-  const filePath = req.params[0];
-  const fullPath = join(STORAGE_PATH, filePath);
-
+// Read
+router.get("/:id", (req, res) => {
+  const {id} = req.params
+  const fileData = filesData.find((file) => file.id === id)
   if (req.query.action === "download") {
-    res.set("Content-Disposition", "attachment");
+    res.set("Content-Disposition", `attachment; filename=${fileData.name}`);
   }
-
-  res.sendFile(fullPath, (err) => {
-    if (err) {
-      res.json({ error: "File not Found" });
+  res.sendFile(`${process.cwd()}/storage/${id}${fileData.extension}`, (err) => {
+    // console.log(err);
+    if (!res.headersSent) {
+      res.json({ error: "File not found!" });
     }
   });
 });
 
-// Update (Rename)
-router.patch("/*", async (req, res) => {
-  const filePath = req.params[0];
-  await rename(join(STORAGE_PATH, filePath), join(STORAGE_PATH, req.body.newFilename));
+// Update
+router.patch("/:id", async (req, res) => {
+  const {id} = req.params;
+  const fileData = filesData.find((file) => file.id === id)
+  fileData.name = req.body.newFilename
+  await writeFile('./filesDB.json', JSON.stringify(filesData))
   res.json({ message: "Renamed" });
 });
 
 // Delete
-router.delete("/*", async (req, res) => {
-  const filePath = req.params[0];
+router.delete("/:id", async (req, res) => {
+  const {id} = req.params
+  const fileIndex = filesData.findIndex((file) => file.id === id)
+  const fileData = filesData[fileIndex]
   try {
-    await rm(join(STORAGE_PATH, filePath), { recursive: true });
+    await rm(`./storage/${id}${fileData.extension}`, { recursive: true });
+    filesData.splice(fileIndex, 1)
+    const parentDirData = directoriesData.find((directoryData) => directoryData.id === fileData.parentDirId)
+    parentDirData.files = parentDirData.files.filter((fileId) => fileId !== id)
+    console.log(parentDirData.files);
+    await writeFile('./filesDB.json', JSON.stringify(filesData))
+    await writeFile('./directoriesDB.json', JSON.stringify(directoriesData))
     res.json({ message: "File Deleted Successfully" });
   } catch (err) {
-    res.status(404).json({ message: "File Not Found!" });
+    res.status(404).json({ message: err.message });
   }
 });
 
